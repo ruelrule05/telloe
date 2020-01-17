d1285ecb3186515511f762f08d37143b2d84c760
Clyde Escobidal <cleidoscope@gmail.com>
2020-01-17 23:33:36 +0800

    update

--- webpack.mix.js
+++ webpack.mix.js
@@ -127,15 +127,15 @@ else if (argv.indexOf('--widget') > -1) {
         .mergeManifest();
     
     mix.browserSync({
-        proxy: 'https://workhob.app',
-        host: 'workhob.app',
+        proxy: 'https://boxbi.app',
+        host: 'boxbi.app',
         open: false,
         port: 8000,
         watch: true,
         notify: false,
         https: {
-            key: '/Users/cleidoscope/.config/valet/Certificates/workhob.app.key',
-            cert: '/Users/cleidoscope/.config/valet/Certificates/workhob.app.crt'
+            key: '/Users/cleidoscope/.config/valet/Certificates/boxbi.app.key',
+            cert: '/Users/cleidoscope/.config/valet/Certificates/boxbi.app.crt'
         }
     });
 }
