9df847c7163de576e9c90eee1f5b4200c793b71e
Clyde Escobidal <cleidoscope@gmail.com>
2020-06-01 18:45:37 +0800

    Update video-call.vue

--- resources/modals/video-call.vue
+++ resources/modals/video-call.vue
@@ -156,8 +156,6 @@ export default {
         	users.push(this.$root.auth);
             let caller = users.find((x) => x.id == this.data.caller);
 
-            console.log(users);
-            console.log(this.data.caller);
             return caller;
         }, 
     },
