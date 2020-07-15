573cfa524cef236b6349701449059073aaada66b
Clyde Escobidal <cleidoscope@gmail.com>
2020-07-15 15:14:11 +0800

    Update dashboard.js

--- Modules/Frontend/Resources/dashboard/dashboard.js
+++ Modules/Frontend/Resources/dashboard/dashboard.js
@@ -142,7 +142,7 @@ window.app = new Vue({
         contentloading: true,
         socket: null,
         online_users: [],
-        detailsTab: 'profile',
+        detailsTab: '',
         profileTab: 'overview', //overview
 
         call_sound: null,
