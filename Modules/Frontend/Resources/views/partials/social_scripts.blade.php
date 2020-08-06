0941fc7ef776ab5664fd1b5ddb70745486061fcf
Clyde Escobidal <cleidoscope@gmail.com>
2020-08-06 14:43:47 +0800

    Update social_scripts.blade.php

--- Modules/Frontend/Resources/views/partials/social_scripts.blade.php
+++ Modules/Frontend/Resources/views/partials/social_scripts.blade.php
@@ -28,3 +28,4 @@
     fjs.parentNode.insertBefore(js, fjs);
   })(document, 'script', 'facebook-jssdk');
 </script>
+<script type="text/javascript" >var EhAPI = EhAPI || {}; EhAPI.after_load = function(){EhAPI.set_account('pma6t5lvi3ehgeemfp6jffdo52', 'telloe');EhAPI.execute('rules');};(function(d,s,f) {var sc=document.createElement(s);sc.type='text/javascript';sc.async=true;sc.src=f;var m=document.getElementsByTagName(s)[0];m.parentNode.insertBefore(sc,m);   })(document, 'script', '//d2p078bqz5urf7.cloudfront.net/jsapi/ehform.js');</script>
