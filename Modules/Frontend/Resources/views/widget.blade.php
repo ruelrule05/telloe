a22eae98862132f75a66b8e5e1759cab3d80df80
Clyde Escobidal <cleidoscope@gmail.com>
2020-08-15 03:33:01 +0800

    Update widget.blade.php

--- Modules/Frontend/Resources/views/widget.blade.php
+++ Modules/Frontend/Resources/views/widget.blade.php
@@ -4,11 +4,5 @@
 </head>
 <body>
 	<script src="https://telloe.com/js/widget.js?p=clydeescobidal"></script>
-	
-
-	<!-- <script>
-		const PROFILE = {!! json_encode($profile) !!};
-	</script>
-	<script src="{{ mix('js/widget.js') }}" async defer></script> -->
 </body>
 </html>
