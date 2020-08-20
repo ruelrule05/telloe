bb3199da5c0eaa6978cd97ed62159ffd1288a53d
Clyde Escobidal <cleidoscope@gmail.com>
2020-08-20 18:16:08 +0800

    Update Helpers.php

--- Modules/Frontend/Http/Helpers.php
+++ Modules/Frontend/Http/Helpers.php
@@ -2,7 +2,7 @@
 
 function compressVideo($source, $ouput)
 {
-    exec('/usr/local/bin/ffmpeg -y -i ' . $source . ' -crf 23 -preset medium -movflags +faststart -b:a 128k -threads 12 -vcodec libx264 -acodec libmp3lame -b:v 1000k -refs 6 -coder 1 -sc_threshold 40 -flags +loop -me_range 16 -subq 7 -i_qfactor 0.71 -qcomp 0.6 -qdiff 4 -trellis 1 -b:a 128k -vf [in]scale=-2:720,format=yuv420p[out] -pass 1 ' . $ouput);
+    echo shell_exec('/usr/local/bin/ffmpeg -y -i ' . $source . ' -crf 23 -preset medium -movflags +faststart -b:a 128k -threads 12 -vcodec libx264 -acodec libmp3lame -b:v 1000k -refs 6 -coder 1 -sc_threshold 40 -flags +loop -me_range 16 -subq 7 -i_qfactor 0.71 -qcomp 0.6 -qdiff 4 -trellis 1 -b:a 128k -vf [in]scale=-2:720,format=yuv420p[out] -pass 1 ' . $ouput);
 }
 
 function checkInviteToken(App\Models\User $user, Illuminate\Http\Request $request, $isNew = false)
