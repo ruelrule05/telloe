3cb0c714e9ea90439fe6092f6af8cfc9d6c2b36f
Clyde Escobidal <cleidoscope@gmail.com>
2020-09-08 20:58:46 +0800

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -70,7 +70,7 @@ class Kernel extends ConsoleKernel
                     endif;
                 endif;
             endforeach;
-        })->everyFiveMinutes();
+        })->everyMinute();
     }
 
     /**
