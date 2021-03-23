e6e7b14e9386ca92da6a9a9a26dba2ccd28bf151
Clyde Escobidal <clyde@tradearcade.tv>
2021-03-23 23:01:15 +0800

    Create 2021_02_24_134131_create_booking_users_table.php

---
+++ database/migrations/2021_02_24_134131_create_booking_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBookingUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('booking_users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('booking_id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('email');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('booking_users');
+    }
+}
