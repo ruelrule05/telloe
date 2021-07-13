1713b54dc6ed7a7c7fa51c23f11395a3dc9fdd18
Mary Sabio <mary@telloe.com>
2021-07-13 14:48:40 +0800

    Create Notes tests

---
+++ tests/Feature/NotesTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class NotesTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testIndex()
+    {
+        $response = $this->actingAs($this->user)->get($this->app_url . '/ajax/notes', $this->headers);
+        $response->assertStatus(200);
+    }
+
+    public function testStore()
+    {
+        $this->conversation = \App\Models\Conversation::first();
+        $conversation_id = $this->conversation->id;
+        $data = [
+            'conversation_id' => $conversation_id,
+            'notes' => 'Test notes'
+        ];
+        $response = $this->actingAs($this->user)->post($this->app_url . "/ajax/notes", $data, $this->headers);
+        $response->assertStatus(200);
+    }
+
+    public function testUpdate()
+    {
+        $this->note = \App\Models\Note::first();
+        $id = $this->note->id;
+        $response = $this->actingAs($this->user)->get($this->app_url . '/ajax/notes/' . $id, $this->headers);
+        $response->assertStatus(200);
+    }
+
+    public function testDelete()
+    {
+        $response = $this->actingAs($this->user)->get($this->app_url . '/ajax/notes/' . $id, $this->headers);
+        $response->assertStatus(200);
+    }
+
+}
