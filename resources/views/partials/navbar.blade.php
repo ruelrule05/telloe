00ee78acf2714a8ead26b1429e6159cf31331414
Clyde Escobidal <cleidoscope@gmail.com>
2021-08-12 14:02:10 +0800

    update

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -27,8 +27,8 @@
         <div>
             <a href="{{ config('app.url') }}"><img class="h-7 w-auto" src="{{ asset('telloe.svg') }}" alt="{{ config('app.name') }}"></a>
         </div>
-        <div>
-          <button type="button" :class="{active: dropdownOpen}" class="navbar-mobile-btn"  v-click-outside="() => (dropdownOpen = false)" @click="dropdownOpen = !dropdownOpen">
+        <div v-click-outside="() => (dropdownOpen = false)">
+          <button type="button" :class="{active: dropdownOpen}" class="navbar-mobile-btn" @click="dropdownOpen = !dropdownOpen">
             <svg class="block h-7 w-7" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
               </svg>
@@ -36,20 +36,20 @@
 
           <div class="navbar-mobile-menu" :class="{open: dropdownOpen}">
              <div class="rounded-xl shadow-md bg-white py-2 px-2">
-                <a href="/">HOME</a>
-                <a href="#features">FEATURES</a>
-                <a href="#about">ABOUT</a>
-                <a href="#pricing">PRICING</a>
-                <a href="/contact">CONTACT US</a>
+                <a href="/" @click="dropdownOpen = false">HOME</a>
+                <a href="/#features" @click="dropdownOpen = false">FEATURES</a>
+                <a href="/#about" @click="dropdownOpen = false">ABOUT</a>
+                <a href="/#pricing" @click="dropdownOpen = false">PRICING</a>
+                <a href="/contact" @click="dropdownOpen = false">CONTACT US</a>
                 @auth
-                <a href="/dashboard/calendar">DASHBOARD</a>
+                <a href="/dashboard/calendar" @click="dropdownOpen = false">DASHBOARD</a>
                 @endauth
                 @guest
                 <div class="px-2 mt-4">
-                    <button type="button" class="btn btn-outline-primary btn-md mb-2 w-full" @click="auth = true; action = 'login'"><span>LOGIN</span></button>
+                    <button type="button" class="btn btn-outline-primary btn-md mb-2 w-full" @click="auth = true; action = 'login'; dropdownOpen = false"><span>LOGIN</span></button>
                 </div>
                 <div class="px-2 pb-2">
-                    <button type="button" class="btn btn-primary btn-md w-full" @click="auth = true; action = 'signup'"><span>SIGN UP</span></button>
+                    <button type="button" class="btn btn-primary btn-md w-full" @click="auth = true; action = 'signup'; dropdownOpen = false"><span>SIGN UP</span></button>
                 </div>
                 @endguest
              </div>
