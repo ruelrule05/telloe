cb9c54f6e699f334b49f32e22141675a6f78f31d
Clyde Escobidal <cleidoscope@gmail.com>
2021-12-13 13:49:58 +0800

    update

---
+++ database/migrations/2021_12_13_153657_edit_fields_in_bookings_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use App\Models\Booking;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class EditFieldsInBookingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        foreach (Booking::whereNotNull('google_event_id')->orWhereNotNull('outlook_event_id')->get() as $booking) {
+            $data = [];
+            if ($booking->google_event_id) {
+                $data['google_event_id'] = json_encode([$booking->google_event_id]);
+            }
+            if ($booking->outlook_event_id) {
+                $data['outlook_event_id'] = json_encode([$booking->outlook_event_id]);
+            }
+            $booking->update($data);
+        }
+        Schema::table('bookings', function (Blueprint $table) {
+            //
+            $table->json('google_event_id')->nullable()->default(NULL)->change();
+            $table->json('outlook_event_id')->nullable()->default(NULL)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('bookings', function (Blueprint $table) {
+            //
+        });
+    }
+}
