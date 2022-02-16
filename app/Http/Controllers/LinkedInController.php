3fd0c0a13168955b3db83104b16c915092a6aadd
Welyelf R. Hisula <32445227+Welyelf@users.noreply.github.com>
2022-02-16 15:23:53 +0800

    created controller for linkedin

---
+++ app/Http/Controllers/LinkedInController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Controller for LinkedIn Integration
+ *
+ * PHP version 7.4.27
+ *
+ * @package    Telloe
+ * @category   Controller
+ * @author     Welyelf Hisula<welyelf@telloe.com>
+ * @copyright  2022 Telloe
+ */
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LinkedInController extends Controller
+{
+    //
+}
