f8914aa85bbb2a9a53a0c6bf6c7d1831c2971452
Welyelf R. Hisula <32445227+Welyelf@users.noreply.github.com>
2022-05-12 19:50:55 +0800

    test gtag on dev

--- resources/views/partials/google_tags.blade.php
+++ resources/views/partials/google_tags.blade.php
@@ -13,3 +13,17 @@
         <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WD8BDT3" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     <!-- End Google Tag Manager (noscript) -->
 @endif
+<!-- Global site tag (gtag.js) - Google Ads: 369673356 -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-369673356"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-369673356');
+</script>
+
+<!-- Event snippet for Sign-up conversion page -->
+<script>
+  gtag('event', 'conversion', {'send_to': 'AW-369673356/_OdCCIibrrwDEIyJo7AB'});
+</script>
