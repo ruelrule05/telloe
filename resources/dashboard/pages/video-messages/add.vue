2bdf99465bcda6492934761f3d899cbf50b74c13
Clyde Escobidal <cleidoscope@gmail.com>
2022-07-21 16:15:50 +0800

    Update add.vue

--- resources/dashboard/pages/video-messages/add.vue
+++ resources/dashboard/pages/video-messages/add.vue
@@ -68,6 +68,7 @@
 							</div>
 							<div class="mb-4">
 								<label>Initial Message</label>
+
 								<input type="file" class="hidden" ref="initialMessageFile" @change="addFile" />
 								<div v-if="videoMessageData.initial_message.message || videoMessageData.initial_message.source" class="flex items-end">
 									<div class="flex-grow overflow-hidden">
@@ -103,9 +104,7 @@
 								<div class="flex items-center mt-2">
 									<div class="flex-grow">
 										<div class="flex items-center rounded-full bg-gray-200 p-1" style="border-radius: 20px">
-											<div class="py-1 px-2 message-input h-auto overflow-auto flex-grow focus:outline-none" @keypress="messageInput" contenteditable data-placeholder="Write a message.." spellcheck="false" ref="messageInput">
-												{{ videoMessageData.retainMessage }}
-											</div>
+											<div class="py-1 px-2 message-input h-auto overflow-auto flex-grow focus:outline-none" @keypress="messageInput" contenteditable data-placeholder="Write a message.." spellcheck="false" ref="messageInput">{{ videoMessageData.retainMessage }}</div>
 											<button type="button" class="btn-send-message rounded-full bg-white p-1.5 text-primary focus:outline-none transition-colors hover:text-white hover:bg-primary" @click="$refs.initialMessageFile.click()">
 												<svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 													<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13" />
@@ -249,6 +248,7 @@ export default {
 			this.$refs.messageInput.innerHTML = videoMessageMessage;
 		}
 	},
+
 	watch: {
 		videoMessage: function () {
 			if (this.videoMessage) {
@@ -307,7 +307,6 @@ export default {
 		setInitialMessage() {
 			let initialMessage = this.$refs.messageInput.innerText || this.videoMessageData.initial_message.message;
 			this.$set(this.videoMessageData.initial_message, 'message', initialMessage);
-			this.$refs.messageInput.innerHTML = '';
 		},
 
 		addFile(e) {
