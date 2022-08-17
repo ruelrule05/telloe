e39e1b070c06eb73a6d57bab58ace3ed6cec29d3
Ruel Rule <ruel@telloe.com>
2022-08-17 12:23:59 +0800

    using mounted property

--- resources/dashboard/pages/video-campaigns/QuickRecorder.vue
+++ resources/dashboard/pages/video-campaigns/QuickRecorder.vue
@@ -88,11 +88,88 @@ export default {
 			this.videoCanvas.style.display = "none";
 			this.$refs.canvasBackground.style.display = "none";
 		}
+
+		this.$nextTick(() => {
+			this.initMediaStreams();
+		});
+		
 	},
 
-	created() {
-		(async () => {
+	methods: {
+		...mapActions({
+			storeUserVideo: 'user_videos/store'
+		}),
 
+		startRecording() {
+			if (this.videoRecorder) {
+				this.isRecording = true;
+				if (this.blobs.length) {
+					this.videoRecorder.resume();
+				} else {
+					this.videoRecorder.start(30);
+					this.recordInterval = setInterval(() => {
+						if (this.isRecording) {
+							this.recordDuration++;
+						}
+					}, 1000);
+				}
+			}
+		},
+		async stopRecording() {
+			this.videoRecorder.pause();
+			const timestamp = dayjs().valueOf();
+			let source = new File(this.blobs, timestamp, {
+				type: this.blobs[0].type
+			});
+			let videoWidth = this.$refs.videoPreview.videoWidth;
+			let videoHeight = this.$refs.videoPreview.videoHeight;
+
+			let thumbnail = null;
+			this.$refs.videoPreview.currentTime = 0;
+			this.$refs.videoPreview.play();
+			const canvas = document.createElement('canvas');
+			canvas.width = 500;
+			canvas.height = (canvas.width / this.$refs.videoPreview.videoWidth) * this.$refs.videoPreview.videoHeight;
+			const ctx = canvas.getContext('2d');
+			if( this.cameraMode == 2 ) {
+				ctx.drawImage(this.videoCanvas, 0, 0, canvas.width, canvas.height);
+			} else {
+				ctx.drawImage(this.$refs.videoPreview, 0, 0, canvas.width, canvas.height);
+			}
+			thumbnail = canvas.toDataURL('image/png');
+			this.$emit('record', { source: source, thumbnail: thumbnail, duration: this.blobs.length * 30 * 2, dimensions: { width: videoWidth, height: videoHeight } });
+
+			this.reset();
+		},
+
+		dataURLtoFile(dataurl, filename) {
+			var arr = dataurl.split(','),
+				mime = arr[0].match(/:(.*?);/)[1],
+				bstr = atob(arr[1]),
+				n = bstr.length,
+				u8arr = new Uint8Array(n);
+
+			while (n--) {
+				u8arr[n] = bstr.charCodeAt(n);
+			}
+
+			return new File([u8arr], filename, { type: mime });
+		},
+
+		secondsToDuration(seconds, limit = 14, end = 5) {
+			let date = new Date(0);
+			date.setSeconds(seconds);
+			let timeString = date.toISOString().substr(limit, end);
+			return timeString;
+		},
+
+		async getVideoSettings() {
+			let response = await window.axios.get('/video-settings')
+			this.videoSettings = response.data;
+			return
+		},
+
+		async initMediaStreams() {
 			this.cameraStreams = await navigator.mediaDevices.getUserMedia({ 
 				video: {
 					width: { min: 1024, ideal: 1280, max: 1920 },
@@ -151,93 +228,6 @@ export default {
 					}
 				};
 			}
-			
-		})();
-	},
-
-	methods: {
-		...mapActions({
-			storeUserVideo: 'user_videos/store'
-		}),
-
-		startRecording() {
-			if (this.videoRecorder) {
-				this.isRecording = true;
-				if (this.blobs.length) {
-					this.videoRecorder.resume();
-				} else {
-					this.videoRecorder.start(30);
-					this.recordInterval = setInterval(() => {
-						if (this.isRecording) {
-							this.recordDuration++;
-						}
-					}, 1000);
-				}
-			}
-		},
-		async stopRecording() {
-			this.videoRecorder.pause();
-			const timestamp = dayjs().valueOf();
-			let source = new File(this.blobs, timestamp, {
-				type: this.blobs[0].type
-			});
-			let videoWidth = this.$refs.videoPreview.videoWidth;
-			let videoHeight = this.$refs.videoPreview.videoHeight;
-
-			let thumbnail = null;
-			this.$refs.videoPreview.currentTime = 0;
-			this.$refs.videoPreview.play();
-			const canvas = document.createElement('canvas');
-			canvas.width = 500;
-			canvas.height = (canvas.width / this.$refs.videoPreview.videoWidth) * this.$refs.videoPreview.videoHeight;
-			const ctx = canvas.getContext('2d');
-			if( this.cameraMode == 2 ) {
-				ctx.drawImage(this.videoCanvas, 0, 0, canvas.width, canvas.height);
-			} else {
-				ctx.drawImage(this.$refs.videoPreview, 0, 0, canvas.width, canvas.height);
-			}
-			thumbnail = canvas.toDataURL('image/png');
-			this.$emit('record', { source: source, thumbnail: thumbnail, duration: this.blobs.length * 30 * 2, dimensions: { width: videoWidth, height: videoHeight } });
-
-			if (this.cameraStreams) {
-				this.cameraStreams.getTracks().forEach(function (track) {
-					track.stop();
-				});
-			}
-			this.cameraStreams = null;
-			this.canvaStream = null;
-			
-			if(this.selfieSegmentation) {
-				this.selfieSegmentation.close();
-				this.selfieSegmentation = null;
-			}
-		},
-
-		dataURLtoFile(dataurl, filename) {
-			var arr = dataurl.split(','),
-				mime = arr[0].match(/:(.*?);/)[1],
-				bstr = atob(arr[1]),
-				n = bstr.length,
-				u8arr = new Uint8Array(n);
-
-			while (n--) {
-				u8arr[n] = bstr.charCodeAt(n);
-			}
-
-			return new File([u8arr], filename, { type: mime });
-		},
-
-		secondsToDuration(seconds, limit = 14, end = 5) {
-			let date = new Date(0);
-			date.setSeconds(seconds);
-			let timeString = date.toISOString().substr(limit, end);
-			return timeString;
-		},
-
-		async getVideoSettings() {
-			let response = await window.axios.get('/video-settings')
-			this.videoSettings = response.data;
-			return
 		},
 
 		init() {
@@ -275,12 +265,24 @@ export default {
 		},
 
 		reset() {
+			if (this.videoRecorder && this.isRecording) {
+				this.videoRecorder.stop();
+			}
+			this.videoRecorder = null;
+			this.isRecording = false;
+			
 			if (this.cameraStreams) {
 				this.cameraStreams.getTracks().forEach(function (track) {
 					track.stop();
 				});
 			}
 			this.cameraStreams = null;
+
+			if (this.canvaStream) {
+				this.canvaStream.getTracks().forEach(function (track) {
+					track.stop();
+				});
+			}
 			this.canvaStream = null;
 
 			if(this.selfieSegmentation) {
